9ccd33b777913ff62a517b01e7cbf39520eeea0c
araguma <itetbitb@gmail.com>
2023-08-06 01:24:20 -0400

    test: Rewrite testing code

--- specs/versions/0.5/holoedit.spec.ts
+++ specs/versions/0.5/holoedit.spec.ts
@@ -1,135 +1,138 @@
+import * as fs from 'node:fs';
 import * as path from 'node:path';
 import * as assert from 'node:assert';
 import HoloEdit from '../../../src/versions/0.5/holoedit';
+import * as save from '../../../src/versions/0.5/save';
 
-const saveLocation = path.join(__dirname, 'saves/');
-const saves = Array(1).fill(null).map((_, i) => `${saveLocation}save-${i + 1}.dat`);
-const randomSave = () => saves[Math.floor(Math.random() * saves.length)];
+const saveDirectory = path.join(__dirname, 'saves');
+const saveFilenames = ['complete.dat', 'fresh.dat'];
+const editors = saveFilenames.map((filename) => {
+    return new HoloEdit(path.join(saveDirectory, filename));
+});
 
-describe('HoloEdit', () => {
+describe('HoloEdit.v0_5', () => {
     describe('#data', () => {
         it('should be an object', () => {
-            const edit = new HoloEdit(randomSave());
-            assert.equal(typeof edit.data, 'object');
+            editors.forEach((editor) => {
+                assert.equal(typeof editor.data, 'object');
+            });
         });
     });
     describe('#achievement()', () => {
-        it('sets the achievement unlocked status', () => {
-            const edit = new HoloEdit(randomSave());
-            edit.achievement('faunaClear', true);
-            assert.equal(edit.data.achievements.faunaClear.unlocked, true);
+        it('should modify only the values of a given achievement', () => {
+            editors.forEach((editor) => {
+                editor.data.achievements.faunaGachikoi = {
+                    unlocked: 0,
+                    flags: {},
+                };
+                editor.data.achievements.hallucinated = {
+                    unlocked: 0,
+                    flags: {},
+                }
+                editor.achievement(save.ACHIEVEMENT.FAUNA_GACHIKOI, 1, { missed: true });
+                assert.equal(editor.data.achievements.faunaGachikoi?.unlocked, 1);
+                assert.equal(editor.data.achievements.faunaGachikoi?.flags.missed, true);
+                assert.equal(editor.data.achievements.hallucinated?.unlocked, 0);
+                assert.equal(Object.entries(editor.data.achievements.hallucinated?.flags ?? {
+                    isUndefined: true,
+                }), 0);
+            });
+        });
+        it('should modify the values of all achievements if the given achievement is \'all\'', () => {
+            editors.forEach((editor) => {
+                editor.data.achievements = {};
+                editor.achievement('all', 0, { test: false });
+                Object.values(save.ACHIEVEMENT).forEach((achievement) => {
+                    assert.equal(editor.data.achievements[achievement]?.unlocked, 0);
+                    assert.equal(editor.data.achievements[achievement]?.flags.test, false);
+                });
+                editor.achievement('all', 1);
+                Object.values(save.ACHIEVEMENT).forEach((achievement) => {
+                    assert.equal(editor.data.achievements[achievement]?.unlocked, 1);
+                    assert.equal(editor.data.achievements[achievement]?.flags.test, false);
+                });
+                editor.data.achievements = {};
+                editor.achievement('all', 0);
+                Object.values(save.ACHIEVEMENT).forEach((achievement) => {
+                    assert.equal(editor.data.achievements[achievement]?.unlocked, 0);
+                    assert.equal(Object.entries(editor.data.achievements[achievement]?.flags ?? {
+                        isUndefined: true,
+                    }), 0);
+                });
+            });
         });
     });
     describe('#coin()', () => {
-        it('sets the coin amount while accounting for randomMoneyKey', () => {
-            const edit = new HoloEdit(randomSave());
-            const amount = Math.floor(Math.random() * 1000);
-            edit.coin(amount);
-            assert.equal(edit.data.holoCoins + edit.data.randomMoneyKey, amount);
+        it('should set the number of coins while accounting for \'randomMoneyKey\'', () => {
+            editors.forEach((editor) => {
+                editor.coin(0);
+                assert.equal(editor.data.holoCoins, -editor.data.randomMoneyKey);
+                editor.coin(1111);
+                assert.equal(editor.data.holoCoins, 1111 - editor.data.randomMoneyKey);
+            });
         });
     });
     describe('#tear()', () => {
-        it('sets the tear amount for the specified generation', () => {
-            const edit = new HoloEdit(randomSave());
-            const amount = Math.floor(Math.random() * 1000);
-            edit.tear('gen1', amount);
-            assert.equal(edit.data.tears.find(t => t[0] === 'gen1')?.[1], amount);
+        it('should modify only the values of a given generation', () => {
+            editors.forEach((editor) => {
+                editor.data.tears = [[save.GENERATION.GEN_0, 0], [save.GENERATION.GEN_1, 0]];
+                editor.tear(save.GENERATION.GEN_0, 1);
+                assert.equal(editor.data.tears[0][1], 1);
+                assert.equal(editor.data.tears[1][1], 0);
+                editor.tear(save.GENERATION.GEN_1, 2);
+                assert.equal(editor.data.tears[0][1], 1);
+                assert.equal(editor.data.tears[1][1], 2);
+            });
         });
-    });
-    describe('#gRank()', () => {
-        it('sets the gRank for the specified character', () => {
-            const edit = new HoloEdit(randomSave());
-            const rank = Math.floor(Math.random() * 1000);
-            edit.gRank('fauna', rank);
-            assert.equal(edit.data.characters.find(c => c[0] === 'fauna')?.[1], rank);
+        it('should modify the values of all generations if the given generation is \'all\'', () => {
+            editors.forEach((editor) => {
+                editor.tear('all', 1);
+                const seenGenerations = Array<string>();
+                editor.data.tears.forEach((generation) => {
+                    assert.equal(generation[1], 1);
+                    seenGenerations.push(generation[0]);
+                });
+                editor.tear('all', 2);
+                editor.data.tears.forEach((generation) => {
+                    assert.equal(generation[1], 2);
+                });
+                const seenGenerationsSet = new Set(seenGenerations);
+                Object.values(save.GENERATION).forEach((generation) => {
+                    assert(seenGenerationsSet.has(generation));
+                });
+            });
         });
     });
     describe('#outfit()', () => {
-        it('adds the outfit to the unlockedOutfits array if unlocked is true', () => {
-            const edit = new HoloEdit(randomSave());
-            edit.outfit('faunaAlt1', true);
-            assert.equal(edit.data.unlockedOutfits.includes('faunaAlt1'), true);
+        it('should modify only the values of a given outfit', () => {
+            editors.forEach((editor) => {
+                editor.data.unlockedOutfits = [];
+                editor.outfit(save.OUTFIT.FAUNA_ALT_1, true);
+                assert.equal(editor.data.unlockedOutfits[0], save.OUTFIT.FAUNA_ALT_1);
+                assert.equal(editor.data.unlockedOutfits.length, 1);
+            });
         });
-        it('removes the outfit from the unlockedOutfits array if unlocked is false', () => {
-            const edit = new HoloEdit(randomSave());
-            edit.outfit('faunaAlt1', false);
-            assert.equal(edit.data.unlockedOutfits.includes('faunaAlt1'), false);
-        });
-    });
-    describe('#stage()', () => {
-        it('adds the stage to the unlockedStages array if unlocked is true', () => {
-            const edit = new HoloEdit(randomSave());
-            edit.stage('STAGE 1', true);
-            assert.equal(edit.data.unlockedStages.includes('STAGE 1'), true);
-        });
-        it('removes the stage from the unlockedStages array if unlocked is false', () => {
-            const edit = new HoloEdit(randomSave());
-            edit.stage('STAGE 1', false);
-            assert.equal(edit.data.unlockedStages.includes('STAGE 1'), false);
-        });
-    });
-    describe('#item()', () => {
-        it('adds the item to the unlockedItems array if unlocked is true', () => {
-            const edit = new HoloEdit(randomSave());
-            edit.item('Halu', true);
-            assert.equal(edit.data.unlockedItems.includes('Halu'), true);
-        });
-        it('removes the item from the unlockedItems array if unlocked is false', () => {
-            const edit = new HoloEdit(randomSave());
-            edit.item('Halu', false);
-            assert.equal(edit.data.unlockedItems.includes('Halu'), false);
-        });
-    });
-    describe('#weapon()', () => {
-        it('adds the weapon to the unlockedWeapons array if unlocked is true', () => {
-            const edit = new HoloEdit(randomSave());
-            edit.weapon('CEOTears', true);
-            assert.equal(edit.data.unlockedWeapons.includes('CEOTears'), true);
-        });
-        it('removes the weapon from the unlockedWeapons array if unlocked is false', () => {
-            const edit = new HoloEdit(randomSave());
-            edit.weapon('CEOTears', false);
-            assert.equal(edit.data.unlockedWeapons.includes('CEOTears'), false);
-        });
-    });
-    describe('#collab()', () => {
-        it('adds the collab to the seenCollabs array if unlocked is true', () => {
-            const edit = new HoloEdit(randomSave());
-            edit.collab('BrokenDreams', true);
-            assert.equal(edit.data.seenCollabs.includes('BrokenDreams'), true);
-        });
-        it('removes the collab from the seenCollabs array if unlocked is false', () => {
-            const edit = new HoloEdit(randomSave());
-            edit.collab('BrokenDreams', false);
-            assert.equal(edit.data.seenCollabs.includes('BrokenDreams'), false);
-        });
-    });
-    describe('#clear()', () => {
-        it('sets the clear amount for the specified character', () => {
-            const edit = new HoloEdit(randomSave());
-            const clears = Math.floor(Math.random() * 1000);
-            edit.clear('fauna', clears);
-            assert.equal(edit.data.characterClears.find(c => c[0] === 'fauna')?.[1], clears);
+        it('should modify the values of all outfits if the given outfit is \'all\'', () => {
+            editors.forEach((editor) => {
+                editor.outfit('all', false);
+                assert.equal(editor.data.unlockedOutfits.length, 0);
+                editor.outfit('all', true);
+                const outfitSet = new Set(editor.data.unlockedOutfits);
+                Object.values(save.OUTFIT).forEach((outfit) => {
+                    assert(outfitSet.has(outfit));
+                });
+            });
         });
     });
     describe('#save()', () => {
-        it('overwrites the original save file if no output path is provided', () => {
-            const randomSavePath = randomSave();
-            const edit = new HoloEdit(randomSavePath);
-            const amount = Math.floor(Math.random() * 1000);
-            edit.coin(amount);
-            edit.save();
-            const newEdit = new HoloEdit(randomSavePath);
-            assert.equal(newEdit.data.holoCoins + newEdit.data.randomMoneyKey, amount);
-        });
-        it('saves to the specified output path if it is provided', () => {
-            const outputPath = path.join(__dirname, 'saves/output.dat');
-            const edit = new HoloEdit(randomSave());
-            const amount = Math.floor(Math.random() * 1000);
-            edit.coin(amount);
-            edit.save(outputPath);
-            const newEdit = new HoloEdit(outputPath);
-            assert.equal(newEdit.data.holoCoins + newEdit.data.randomMoneyKey, amount);
+        it('should save the changes to the given path', () => {
+            editors.forEach((editor) => {
+                const saveLocation = path.join(saveDirectory, 'temp.dat');
+                fs.rmSync(saveLocation, { force: true });
+                editor.save(saveLocation);
+                const data = new HoloEdit(saveLocation).data;
+                assert.deepEqual(data, editor.data);
+            });
         });
     });
 });
